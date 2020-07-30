dbf2afdd1b90f5fbc02805152098db002e2c9ad2
Will Hart <hart.wl@gmail.com>
2020-07-30 19:30:36 +1000

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# 2d-navmesh-experiment
+# 2d NavMesh Experiment
+
+Trying out a couple of different approaches for building up
+2D navmeshes from 2D grids. The objective is not necessarily
+to get the "best" solution, but one that works in reasonable
+time and with reasonable accuracy.
+
+Open in CodeBox or Vercel (https://csb-vz5jc-2ch0kpug0.vercel.app/)
+to try random maps or draw your own and see it update live.
+
+NavMesh building on a 40x40 grid takes ~0.2ms. Pathfinding takes
+1-5ms depending on the obstacle density.
+
+This example is a 40x40 grid with obstacle density (set in `constants.ts`)
+of 0.5
+
+![Mesh navigation example](public/mesh_navigation_example.png)
+
 Created with CodeSandbox
--- src/NavMeshBuilder.ts
+++ src/NavMeshBuilder.ts
@@ -1,7 +1,7 @@
 import NavMesh from 'navmesh'
-import { NavMesh as NavMesh2 } from 'nav2d'
+// import { NavMesh as NavMesh2 } from 'nav2d'
 
-import { Poly, PolyPoint, PolyArrayPoint } from './gridBuilder'
+import { Poly, PolyPoint } from './gridBuilder'
 
 const polyMapper = (poly: Poly): PolyPoint[] => [
   { x: poly.x, y: poly.y },
@@ -20,19 +20,19 @@ export const getNavMesh = (
   return navMesh.findPath(from, to)
 }
 
-const polyMapper2 = (poly: Poly): PolyArrayPoint[] => [
-  [poly.x, poly.y],
-  [poly.x + poly.width, poly.y],
-  [poly.x + poly.width, poly.y + poly.height],
-  [poly.x, poly.y + poly.height],
-]
+// const polyMapper2 = (poly: Poly): PolyArrayPoint[] => [
+//   [poly.x, poly.y],
+//   [poly.x + poly.width, poly.y],
+//   [poly.x + poly.width, poly.y + poly.height],
+//   [poly.x, poly.y + poly.height],
+// ]
 
-export const getNavMesh2 = (
-  polys: Poly[],
-  from: PolyPoint,
-  to: PolyPoint,
-): PolyPoint[] => {
-  const mesh = polys.map(polyMapper2)
-  const navMesh = new NavMesh2(mesh)
-  return navMesh.findPath([from.x, from.y], [to.x, to.y])
-}
+// export const getNavMesh2 = (
+//   polys: Poly[],
+//   from: PolyPoint,
+//   to: PolyPoint,
+// ): PolyPoint[] => {
+//   const mesh = polys.map(polyMapper2)
+//   const navMesh = new NavMesh2(mesh)
+//   return navMesh.findPath([from.x, from.y], [to.x, to.y])
+// }
